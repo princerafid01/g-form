e3669c03e9847895b6c9088152f6a1a011e931fa
Rafid <mahmudrafid02@gmail.com>
2020-08-09 01:09:54 +0600

    Project initialized

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import GAuth from 'vue-google-oauth2'
 
 Vue.config.productionTip = false
 
+const gauthOption = {
+  clientId: '124613989619-of8821u4cq8f6bdqbp065nqm00e56utm.apps.googleusercontent.com',
+  scope: 'email',
+  prompt: 'consent',
+  fetch_basic_profile: false
+}
+
+Vue.use(GAuth, gauthOption)
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
